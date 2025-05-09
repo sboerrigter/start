77286dbbb5608716e82d83182e4d901d6725f834
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-05-09 22:03:54 +0200

    Dit was het he?

--- web/index.php
+++ web/index.php
@@ -1,11 +1,5 @@
 <?php
 $uri = $_SERVER['REQUEST_URI'];
 
-// Redirect /wp-admin and /wp-login.php to WordPress
-if (strpos($uri, '/wp-admin') === 0 || strpos($uri, '/wp-login.php') === 0) {
-  header("Location: /wp{$uri}");
-  exit();
-}
-
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp/wp-blog-header.php';
