22efa9e041a07f40d2ebecb7079fda4ccdddb885
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-05-16 14:18:36 +0200

    🔥 Theme copy

--- public/theme/scripts/editor.js
+++
@@ -1,8 +0,0 @@
-wp.domReady(function () {
-  // Unregister block styles
-  wp.blocks.unregisterBlockStyle('core/image', 'default');
-  wp.blocks.unregisterBlockStyle('core/image', 'rounded');
-  wp.blocks.unregisterBlockStyle('core/quote', 'default');
-  wp.blocks.unregisterBlockStyle('core/quote', 'plain');
-  wp.blocks.unregisterBlockStyle('core/separator', 'dots');
-});
