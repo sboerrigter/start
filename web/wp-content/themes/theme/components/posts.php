ed49283c363da484c057150f5fd6dcbc08c9d7b4
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-05-26 17:37:59 +0200

    🚧 Not found message

--- web/wp-content/themes/theme/components/posts.php
+++ web/wp-content/themes/theme/components/posts.php
@@ -2,12 +2,18 @@
 use Theme\PostTypes\Post;
 ?>
 
-<div class="grid gap-10 sm:grid-cols-2 md:gap-15 lg:grid-cols-3">
-  <?php while ( have_posts() ) { ?>
-    <?php the_post(); ?>
+<?php if (have_posts()) { ?>
+  <div class="grid gap-10 sm:grid-cols-2 md:gap-15 lg:grid-cols-3">
+    <?php while ( have_posts() ) { ?>
+      <?php the_post(); ?>
 
-    <?= component('post', ['post' => new Post(get_the_ID())]); ?>
-  <?php } ?>
-</div>
+      <?= component('post', ['post' => new Post(get_the_ID())]); ?>
+    <?php } ?>
+  </div>
+<?php } else { ?>
+  <p>
+    <?= __('No articles were found', 'theme'); ?>.
+  </p>
+<?php } ?>
 
 <?= component('pagination'); ?>
