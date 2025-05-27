bc86fe2cb2f85d9e5efaa21d931acb18d9579b1e
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-05-27 16:13:16 +0200

    ⚡ Eager load header image

--- web/wp-content/themes/theme/components/hero.php
+++ web/wp-content/themes/theme/components/hero.php
@@ -5,9 +5,10 @@
         'rounded-lg object-cover',
         is_front_page() ? 'max-h-[600px]' : 'max-h-[400px]',
       ]),
+      'height' => 900,
+      'loading' => 'eager',
       'src' => $image,
       'width' => 1200,
-      'height' => 900,
       'widths' => [335, 480, 600, 1200],
     ]); ?>
   <?php } else { ?>
