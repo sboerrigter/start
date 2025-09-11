71e15a009382a2164cd7b1bf58b156d55cd15879
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-09-11 15:54:59 +0200

    💄 Support multiple levels in menu

--- web/wp-content/themes/theme/components/menu-item.php
+++ web/wp-content/themes/theme/components/menu-item.php
@@ -1,20 +1,30 @@
-<div
-  class="
-    menu-item group relative
-    <?= !isset($isChild) ?
-      'max-lg:border-t max-lg:last:border-b border-white/25 font-semibold' :
-      'font-normal'
-    ?>
-    <?= $item->isCurrent || $item->isParent ? 'menu-item-open' : ''?>
-  "
->
+<?php
+// Define group classes based on level
+// We write them fully here, to make sure they end up in the CSS ouput
+if ($level == 1) {
+  $group = 'group/level1';
+  $groupHover = 'lg:group-hover/level1:flex';
+} elseif ($level == 2) {
+  $group = 'group/level2';
+  $groupHover = 'lg:group-hover/level2:flex';
+} else {
+  $group = 'group/level3';
+  $groupHover = 'lg:group-hover/level3:flex';
+}
+?>
+
+<div class="
+    menu-item <?= $group; ?> relative
+    <?= !isset($isChild) ? 'max-lg:border-t max-lg:last:border-b border-white/25 font-semibold' : 'font-normal'; ?>
+    <?= $item->isCurrent || $item->isParent ? 'menu-item-open' : ''; ?>
+  ">
   <div class="flex">
     <a
       class="
         flex gap-1 items-center max-lg:w-full no-underline text-white/75 hover:text-white/100
         <?= !isset($isChild) ?
           'p-3 lg:px-4 lg:py-3 lg:text-gray-800 lg:hover:text-primary-600' :
-          'p-1 lg:px-4 lg:w-full'
+          'p-1 lg:px-4 lg:w-full';
         ?>
       "
       href="<?= $item->url; ?>"
@@ -24,30 +34,39 @@
 
       <?php if ($item->children) { ?>
         <?= component('svg/chevron-down', [
-          'class' => 'hidden lg:block w-5 h-5',
+          'class' => implode(' ', [
+            'hidden lg:block w-5 h-5',
+            ($level > 1) ? '-rotate-90' : '',
+          ]),
         ]); ?>
       <?php } ?>
     </a>
 
     <?php if ($item->children) { ?>
-    <div
-      class="menu-item-toggle user-select-none flex h-12 w-12 -rotate-90 cursor-pointer items-center justify-center text-white/50 transition-all hover:text-white/100 lg:hidden"
-    >
-      <?= component('svg/chevron-down', ['class' => 'w-5 h-5']); ?>
-    </div>
+      <div class="
+        menu-item-toggle user-select-none flex h-12 w-12 -rotate-90 cursor-pointer items-center justify-center text-white/50 transition-all hover:text-white/100 lg:hidden
+      ">
+        <?= component('svg/chevron-down', ['class' => 'w-5 h-5']); ?>
+      </div>
     <?php } ?>
   </div>
 
   <?php if ($item->children) { ?>
-  <div
-    class="menu-item-children hidden flex-col max-lg:mb-5 max-lg:ml-5 lg:absolute lg:left-0 lg:w-60 lg:rounded lg:bg-primary-900 lg:py-3 lg:group-last:right-0 lg:group-last:left-auto lg:group-hover:flex"
-  >
-    <?php foreach ($item->children as $item) { ?>
+    <div class="
+      menu-item-children hidden flex-col
+      max-lg:mb-5 max-lg:ml-5
+      lg:absolute lg:left-0 lg:w-60 lg:rounded lg:bg-primary-900 lg:py-3
+      lg:group-last:right-0 lg:group-last:left-auto
+      <?= $groupHover; ?>
+      <?= ($level > 1) ? 'lg:left-full lg:top-0' : 'lg:left-0' ?>
+    ">
+      <?php foreach ($item->children as $item) { ?>
         <?= component('menu-item', [
           'item' => $item,
           'isChild' => true,
+          'level' => $level + 1,
         ]); ?>
       <?php } ?>
-  </div>
+    </div>
   <?php } ?>
 </div>
--- web/wp-content/themes/theme/components/menu.php
+++ web/wp-content/themes/theme/components/menu.php
@@ -1,7 +1,12 @@
-<div
-  class="menu top-0 left-0 z-10 flex flex-col max-lg:pointer-events-none max-lg:absolute max-lg:h-screen max-lg:w-full max-lg:-translate-y-full max-lg:overflow-auto max-lg:bg-primary-900 max-lg:p-5 max-lg:pt-20 max-lg:opacity-0 max-lg:transition-opacity lg:flex-row"
->
+<div class="
+  menu top-0 left-0 z-10 flex flex-col
+  max-lg:pointer-events-none max-lg:absolute max-lg:h-screen max-lg:w-full max-lg:-translate-y-full max-lg:overflow-auto max-lg:bg-primary-900 max-lg:p-5 max-lg:pt-20 max-lg:opacity-0 max-lg:transition-opacity
+  lg:flex-row lg:flex-wrap lg:justify-end
+">
   <?php foreach (Theme\Menu::items('main') as $item) { ?>
-    <?= component('menu-item', ['item' => $item]); ?>
+    <?= component('menu-item', [
+      'item' => $item,
+      'level' => 1,
+    ]); ?>
   <?php } ?>
 </div>
