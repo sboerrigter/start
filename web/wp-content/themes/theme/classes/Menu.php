09288b09e7a6eac3050244b0a852c6b7cce4cc58
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2025-12-17 11:24:30 +0100

    ♻️ Menu class

--- web/wp-content/themes/theme/classes/Menu.php
+++ web/wp-content/themes/theme/classes/Menu.php
@@ -4,6 +4,7 @@ namespace Theme;
 
 class Menu
 {
+  private $menu;
   private static $currentUrl;
 
   public static function init()
@@ -19,6 +20,27 @@ class Menu
     register_nav_menu('footer', __('Footer menu', 'theme'));
   }
 
+  // Get menu and save menu object by menu location
+  public function __construct(?string $location = null)
+  {
+    // Bail if location is not provided
+    if (!$location) {
+      $this->menu = null;
+      return;
+    }
+
+    // Get all menu locations and check if the location exists
+    $locations = get_nav_menu_locations();
+
+    if (!in_array($location, array_keys($locations))) {
+      $this->menu = null;
+      return;
+    }
+
+    // Get menu object by location
+    $this->menu = wp_get_nav_menu_object($locations[$location]);
+  }
+
   // Set current URL so we can determine the current and parent menu pages
   public static function setCurrentUrl()
   {
@@ -26,39 +48,37 @@ class Menu
     static::$currentUrl = trailingslashit(home_url($wp->request));
   }
 
-  // Get menu items by nav menu location
-  public static function items(string $location = 'main')
+  // Get menu title
+  public function name()
   {
-    $locations = get_nav_menu_locations();
+    return $this->menu ? $this->menu->name : '';
+  }
 
-    // Return empty array if menu location doesn't exist
-    if (!in_array($location, array_keys($locations))) {
-      return [];
-    }
-
-    $menu = wp_get_nav_menu_object($locations[$location]);
-    $items = wp_get_nav_menu_items($menu->name);
+  // Get menu items
+  public function items()
+  {
+    $items = wp_get_nav_menu_items($this->menu->name);
 
     // Return empty array if menu is empty
     if (!$items || count($items) == 0) {
       return [];
     }
 
-    $items = static::addIsCurent($items);
-    $items = static::buildTree($items, 0);
-    $items = static::addIsParent($items);
+    $items = $this->addIsCurrent($items);
+    $items = $this->buildTree($items, 0);
+    $items = $this->addIsParent($items);
 
-    return $items;
+    return $items ?: [];
   }
 
   // Build a nested array from a flat menu items array
-  private static function buildTree(array &$items, $parentId = 0)
+  private function buildTree(array &$items, $parentId = 0)
   {
     $output = [];
 
     foreach ($items as &$item) {
       if ($item->menu_item_parent == $parentId) {
-        $children = static::buildTree($items, $item->ID);
+        $children = $this->buildTree($items, $item->ID);
 
         if ($children) {
           $item->children = $children;
@@ -72,7 +92,7 @@ class Menu
   }
 
   // Add isCurrent to menu item
-  private static function addIsCurent($items)
+  private function addIsCurrent($items)
   {
     $items = array_map(function ($item) {
       $item->isCurrent = $item->url === static::$currentUrl;
@@ -84,33 +104,35 @@ class Menu
   }
 
   // Add isParent to parent menu items
-  private static function addIsParent($items)
+  private function addIsParent($items)
   {
     if (!$items) {
-      return null;
+      return [];
     }
 
     return array_map(function ($item) {
       $urls = [];
 
       if ($item->children) {
-        $urls = array_merge($urls, static::getSubitemUrls($item));
+        $urls = array_merge($urls, $this->getSubitemUrls($item));
       }
 
       $item->isParent = $item->children && in_array(static::$currentUrl, $urls);
-      $item->children = static::addIsParent($item->children);
+      $item->children = $this->addIsParent($item->children);
 
       return $item;
     }, $items);
   }
 
-  private static function getSubitemUrls($item)
+  private function getSubitemUrls($item)
   {
+    $urls = [];
+
     foreach ($item->children as $subitem) {
       $urls[] = $subitem->url;
 
       if ($subitem->children) {
-        $urls = array_merge($urls, static::getSubitemUrls($subitem));
+        $urls = array_merge($urls, $this->getSubitemUrls($subitem));
       }
     }
 
--- web/wp-content/themes/theme/components/footer.php
+++ web/wp-content/themes/theme/components/footer.php
@@ -1,24 +1,32 @@
-    <footer class="
-      wrapper py-6 flex items-center gap-2 text-center flex-col
-      lg:py-8 lg:flex-row lg:justify-between
-    ">
-      <div class="px-4 py-1">
-        &copy; <?= date('Y'); ?> - <?= get_bloginfo('name'); ?>
-      </div>
+<?php
+use Theme\Menu;
 
-      <div class="flex flex-wrap justify-center items-end">
-        <?php foreach (Theme\Menu::items('footer') as $item) { ?>
-          <a
-            class="px-4 py-1 font-semibold no-underline text-gray-800 hover:text-primary-600"
-            href="<?= $item->url; ?>"
-            target="<?= $item->target; ?>"
-          >
-            <?= $item->title; ?>
-          </a>
-        <?php } ?>
-      </div>
-    </footer>
+$menu = new Menu('footer');
+?>
 
-    <?php wp_footer(); ?>
-  </body>
+<footer class="
+  wrapper py-6 flex items-center gap-2 text-center flex-col
+  lg:py-8 lg:flex-row lg:justify-between
+">
+  <div class="px-4 py-1">
+    &copy; <?= date('Y'); ?> - <?= get_bloginfo('name'); ?>
+  </div>
+
+  <div class="flex flex-wrap justify-center items-end">
+    <?php foreach ($menu->items() as $item) { ?>
+      <a
+        class="px-4 py-1 font-semibold no-underline text-gray-800 hover:text-primary-600"
+        href="<?= $item->url; ?>"
+        target="<?= $item->target; ?>"
+      >
+        <?= $item->title; ?>
+      </a>
+    <?php } ?>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+
 </html>
